8d1d0b45cca0cb054a422fc7987192755c1417b9
Stefaan <safwbdev@gmail.com>
2026-07-12 20:55:18 +0800

    fix typo

--- src/data.js
+++ src/data.js
@@ -153,7 +153,7 @@ export const PROJECTS_PERSONAL = [
     {
         num: '07', title: 'Aura',
         desc: 'Animated web experience recreating the iconic opening sequence of Doctor Who using Three.js. Features real-time 3D tunnel effects, particle systems, dynamic camera movement, and immersive visual storytelling rendered entirely in the browser.',
-        tags: ['ThreeJS', GSAP],
+        tags: ['ThreeJS', 'GSAP'],
         github: 'https://github.com/safwbdev/aura-ring', demo: 'https://auraairing.netlify.app/',
         thumb: aura,
     },
